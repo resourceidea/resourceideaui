81327fba94ef81b52a5448abb4f5e62be232b7fe
Joseph Serunjogi <joeseggie@gmail.com>
2019-09-11 13:42:49 +0300

    Adding banner

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,32 @@
 <template>
   <div id="app">
     <PublicNavbar />
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container-fluid body-content">
+      <Banner />
+      <img alt="Vue logo" src="./assets/logo.png" />
+      <HelloWorld msg="Welcome to Your Vue.js App" />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 import PublicNavbar from "./components/PublicNavbar";
+import Banner from "./components/Banner";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     HelloWorld,
-    PublicNavbar
+    PublicNavbar,
+    Banner
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
