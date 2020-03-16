2e2e57135774ae0fe5469ff3adcd04cb8aef82e2
Joseph Serunjogi <joeseggie@gmail.com>
2020-03-16 17:35:42 +0300

    Remove pages from folder structure

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,15 +1,8 @@
 import React, { Component } from "react";
 
 class Dashboard extends Component {
-    render() {  
-  
-        return (  
-            <div class="row" className="mb-2 pageheading">  
-                <div class="col-sm-12 btn btn-primary">  
-                    Dashboard   
-             </div>  
-            </div>  
-        );  
+    render() {
+        return <h3>Dashboard</h3>
     }
 }
 
