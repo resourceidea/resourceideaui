ab5fc08f0b19edfdf6eb3d604455d0daa69b3628
Muganji Julius <muganji93@gmail.com>
2020-03-30 22:46:48 +0300

    Creation of Login

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Home from './components/home/Home';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Nav from './components/common/nav/Nav';
+import ResetPassword from "./components/resetpassword/ResetPassword";
+import Login from "./components/login/Login";
 import Dashboard from "./components/dashboard/Dashboard";
 import Admin from './components/admin/Admin';
 import ProfileDetail from './components/profile/ProfileDetail';
@@ -14,6 +16,8 @@ const App = () => {
         <Nav />
         <Switch>
           <Route path='/' exact component={Home} />
+          <Route path="/login/reset" exact component={ResetPassword}/>
+          <Route path='/login' component={Login} />
           <Route path='/dashboard' component={Dashboard} />
           <Route path='/admin' component={Admin} />
           <Route path='/profile' component={ProfileDetail} />
