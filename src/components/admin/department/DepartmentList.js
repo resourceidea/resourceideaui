ff5746cd001573b1415764f42440da9cec502ed5
Joseph Serunjogi <joeseggie@gmail.com>
2020-05-07 22:20:21 +0300

    Correct links on the departments list page

--- src/components/admin/department/DepartmentList.js
+++ src/components/admin/department/DepartmentList.js
@@ -5,8 +5,8 @@ class DepartmentsList extends Component {
     render() {
         return (
             <div className='text-left'>
-                <Link to='/admin/clients' className='btn btn-light btn-sm text-left'>Dashboard</Link>&nbsp;&nbsp;
-                <Link to='/admin/clients/add' className='btn btn-light btn-sm'>Add new client</Link>
+                <Link to='/admin/departments' className='btn btn-light btn-sm text-left'>Dashboard</Link>&nbsp;&nbsp;
+                <Link to='/admin/departments/add' className='btn btn-light btn-sm'>Add new client</Link>
                 <br /><br />
                 <h5>Departments list</h5>
                 <hr />
