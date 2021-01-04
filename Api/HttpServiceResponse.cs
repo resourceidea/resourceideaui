e93bf0df8e3533636f6841da6256c99490875f89
Joseph Serunjogi <jserunjogi@touchpointmed.com>
2021-01-04 18:41:28 +0300

    List and add department through proxy Azure Function

---
+++ Api/HttpServiceResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace Api
+{
+    struct HttpServiceResponse
+    {
+        public HttpStatusCode StatusCode;
+        public string Content;
+    }
+}
