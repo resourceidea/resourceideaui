c88d870ba411c1fb4e26d8ad98e2cb49d2961e96
Joseph Serunjogi <jserunjogi@touchpointmed.com>
2021-01-15 21:10:19 +0300

    Add function to return all job positions

---
+++ Api/HttpRequestHandler.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Primitives;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api
+{
+    public static class HttpRequestHandler
+    {
+        public static async Task<HttpResponseMessage> Process(HttpRequest request, ILogger logger, string id)
+        {
+            request.Headers.TryGetValue("Authorization", out StringValues authorizationHeaderValues);
+            string authToken = authorizationHeaderValues.ToString().Split(' ')[1];
+
+            try
+            {
+                if (request.Method == HttpMethod.Post.Method)
+                {
+                    return await HandlePostRequest(request, authToken, logger);
+                }
+                else if (request.Method == HttpMethod.Put.Method)
+                {
+                    return await HandlePutRequest(request, authToken, logger, id);
+                }
+                else
+                {
+                    return await HandleGetRequest(request, authToken, logger, id);
+                }
+            }
+            catch (HttpRequestException error)
+            {
+                logger.LogError($"ERROR: Departments request failed with error {error.Message}");
+
+                return new HttpResponseMessage(HttpStatusCode.BadRequest)
+                {
+                    Content = new StringContent(JsonConvert.SerializeObject(new { message = ConstantValues.DEPARTMENTS_REQUEST_FAILED }),
+                                                Encoding.UTF8,
+                                                "application/json")
+                };
+            }
+        }
+
+        private static async Task<HttpResponseMessage> HandleGetRequest(HttpRequest request, string token, ILogger log, string id)
+        {
+            string page = request.Query["page"];
+            var queryPage = string.IsNullOrEmpty(page) ? string.Empty : $"?page={page}";
+            var uri = string.IsNullOrEmpty(id) ? $"/jobpositions/{queryPage}" : $"/jobpositions/{id}/";
+
+            var _httpService = new HttpService();
+            var serviceResponse = await _httpService.Get(uri, token);
+
+            HttpResponseMessage responseMessage = new HttpResponseMessage(serviceResponse.StatusCode);
+
+            if (serviceResponse.StatusCode == HttpStatusCode.OK)
+            {
+                responseMessage.Content = new StringContent(serviceResponse.Content,
+                                                            Encoding.UTF8,
+                                                            "application/json");
+
+                log.LogInformation($"INFO: Job position(s) resource request successful");
+            }
+            else
+            {
+                log.LogError($"ERROR: Job position(s) resource request FAILED with response {serviceResponse.Content}");
+
+                responseMessage.Content = new StringContent(JsonConvert.SerializeObject(new { message =  ConstantValues.JOB_POSITIONS_REQUEST_FAILED }),
+                                                            Encoding.UTF8,
+                                                            "application/json");
+            }
+
+            return responseMessage;
+        }
+
+        private static Task<HttpResponseMessage> HandlePutRequest(HttpRequest request, string token, ILogger log, string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static Task<HttpResponseMessage> HandlePostRequest(HttpRequest request, string token, ILogger log)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Api/JobPositions.cs
+++ Api/JobPositions.cs
@@ -7,29 +7,24 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Microsoft.Extensions.Primitives;
+using System.Net.Http;
+using System.Net;
+using System.Text;
 
 namespace Api
 {
     public static class JobPositions
     {
         [FunctionName("JobPositions")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
+        public static async Task<HttpResponseMessage> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", "put", Route = "jobpositions/{id?}")] HttpRequest request,
+            string id,
+            ILogger logger)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            logger.LogInformation("Job positions resource request.");
 
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
+            return await HttpRequestHandler.Process(request, logger, id);
         }
     }
 }
