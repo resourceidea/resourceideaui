4056872e42e4759d94852adf2ba8dac57b108d41
Joseph Serunjogi <jserunjogi@touchpointmed.com>
2021-01-16 13:03:20 +0300

    Add extension methods for HttpRequest and String in-built classes

---
+++ Api/ExtensionMethods/HttpRequestExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Api.ExtensionMethods
+{
+    public static class HttpRequestExtensions
+    {
+        public static string GetPageFromQueryString(this HttpRequest httpRequest)
+        {
+            return httpRequest.Query["page"];
+        }
+
+        public static string GetResourceFromPath(this HttpRequest httpRequest)
+        {
+            return httpRequest.Path.Value.Split('/')[2];
+        }
+    }
+}
--- Api/Factories/HttpGETMethodHandler.cs
+++ Api/Factories/HttpGETMethodHandler.cs
@@ -1,8 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Api.ExtensionMethods;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Text;
@@ -12,15 +11,21 @@ namespace Api.Factories
 {
     public class HttpGETMethodHandler : IHttpMethodHandlerFactory
     {
+        private readonly HttpService _httpService;
+
+        public HttpGETMethodHandler()
+        {
+            _httpService = new HttpService();
+        }
+
         public async Task<HttpResponseMessage> HandleRequest(HttpRequest request, string authenticationToken, ILogger logger, string id = null)
         {
-            string resource = request.Path.Value.Split('/')[2];
-            string page = request.Query["page"];
+            var resource = request.GetResourceFromPath();
+            var page = request.GetPageFromQueryString();
 
             var queryPage = string.IsNullOrEmpty(page) ? string.Empty : $"?page={page}";
             var uri = string.IsNullOrEmpty(id) ? $"/{resource}/{queryPage}" : $"/{resource}/{id}/";
 
-            var _httpService = new HttpService();
             var serviceResponse = await _httpService.Get(uri, authenticationToken);
 
             HttpResponseMessage responseMessage = new HttpResponseMessage(serviceResponse.StatusCode);
@@ -28,12 +33,16 @@ namespace Api.Factories
             if (serviceResponse.StatusCode == HttpStatusCode.OK)
             {
                 responseMessage.Content = new StringContent(serviceResponse.Content, Encoding.UTF8, "application/json");
-                logger.LogInformation($"INFO: Job position(s) resource request successful");
+                logger.LogInformation($"INFO: {resource} resource request successful");
             }
             else
             {
-                logger.LogError($"ERROR: Job position(s) resource request FAILED with response {serviceResponse.Content}");
-                responseMessage.Content = new StringContent(JsonConvert.SerializeObject(new { message = ConstantValues.JOB_POSITIONS_REQUEST_FAILED }), Encoding.UTF8, "application/json");
+                logger.LogError($"ERROR: {resource } resource request FAILED with response {serviceResponse.Content}");
+                responseMessage.Content = new StringContent(
+                    JsonConvert.SerializeObject(new { message = ConstantValues.JOB_POSITIONS_REQUEST_FAILED }),
+                    Encoding.UTF8,
+                    "application/json"
+                );
             }
 
             return responseMessage;
