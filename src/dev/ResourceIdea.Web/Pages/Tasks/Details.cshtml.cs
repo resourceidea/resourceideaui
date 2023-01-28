d427be30759e181f4533c18fbeb7b70997e334c2
Joseph Serunjogi <joeseggie@gmail.com>
2023-01-28 11:52:02 +0300

    Separate common resources and add tasks management pages. (#73)
    * Migrate from .NET 6.0 to .NET 7.0 (#71)
    
    Co-authored-by: Joseph Serunjogi <jserunjogi@microsoft.com>
    
    * Cleanup routes
    
    * Restructure the project
    
    * Update engagement details
    
    * Separate common resources
    
    * Setup task pages
    
    * Correct solution setup
    
    ---------
    
    Co-authored-by: Joseph Serunjogi <jserunjogi@microsoft.com>

---
+++ src/dev/ResourceIdea.Web/Pages/Tasks/Details.cshtml.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using ResourceIdea.Pages;
+
+namespace ResourceIdea.Web.Pages.Tasks
+{
+    public class TaskDetailsModel : BasePageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ src/dev/ResourceIdea.Web/Pages/Tasks/Index.cshtml.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using ResourceIdea.Pages;
+
+namespace ResourceIdea.Web.Pages.Tasks
+{
+    public class TasksIndexModel : BasePageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
