29e5f679f5ebe986b5e0a9a07716fc33056ba79b
Joseph Serunjogi <jserunjogi@microsoft.com>
2023-01-28 11:33:23 +0300

    Setup task pages

---
+++ src/dev/ResourceIdea.Web/Pages/Tasks/Add.cshtml.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using ResourceIdea.Pages;
+
+namespace ResourceIdea.Web.Pages.Tasks
+{
+    public class AddTaskModel : BasePageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
