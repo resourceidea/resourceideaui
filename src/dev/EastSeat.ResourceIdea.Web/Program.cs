9360a238f9cd5178fd350b572014b70a59ddaf1b
Joseph Serunjogi <joeseggie@gmail.com>
2023-11-07 14:01:57 +0300

    Install .NET 8 framework

--- src/dev/EastSeat.ResourceIdea.Web/Program.cs
+++ src/dev/EastSeat.ResourceIdea.Web/Program.cs
@@ -3,15 +3,15 @@ using EastSeat.ResourceIdea.Web.Components;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+// builder.Services.AddRazorComponents().AddInteractiveServerComponents();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseExceptionHandler("/Error");
+    // app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
@@ -19,9 +19,8 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
-app.UseAntiforgery();
+// app.UseAntiforgery();
 
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+// app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
 
 app.Run();
