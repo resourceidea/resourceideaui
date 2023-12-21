6327470aa1dac7343bdf6f0f1a82c7e58f63f470
Joseph Serunjogi <joeseggie@gmail.com>
2023-12-21 16:06:58 +0300

    Resolve PR review comments from coderabbitai
    Resolved comments from coderabbitat on missing assertions in the unit tests, use of the null-condition operation and the use of the `It.IsAny<T>()` method.

--- src/test/EastSeat.ResourceIdea.Application.Tests/Features/Client/GetClientsListQueryHandlerTests.cs
+++ src/test/EastSeat.ResourceIdea.Application.Tests/Features/Client/GetClientsListQueryHandlerTests.cs
@@ -31,9 +31,9 @@ public class GetClientsListQueryHandlerTests
     {
         // Arrange
         List<Domain.Entities.Client> mockClientList = [
-            It.IsAny<Domain.Entities.Client>(),
-            It.IsAny<Domain.Entities.Client>(),
-            It.IsAny<Domain.Entities.Client>()
+            new Domain.Entities.Client { Id = Guid.NewGuid(), Name = "Client 1", Address = "Address 1", ColorCode = "#000000", SubscriptionId = Guid.NewGuid() },
+            new Domain.Entities.Client { Id = Guid.NewGuid(), Name = "Client 2", Address = "Address 2", ColorCode = "#000000", SubscriptionId = Guid.NewGuid() },
+            new Domain.Entities.Client { Id = Guid.NewGuid(), Name = "Client 3", Address = "Address 3", ColorCode = "#000000", SubscriptionId = Guid.NewGuid() }
         ];
         var clientRepository = new Mock<IAsyncRepository<Domain.Entities.Client>>();
         clientRepository.Setup(repo => repo.ListAllAsync()).ReturnsAsync(() => mockClientList);
@@ -46,5 +46,8 @@ public class GetClientsListQueryHandlerTests
         // Assert
         Assert.IsType<List<ClientListDTO>>(result);
         Assert.Equal(3, result.Count);
+        Assert.Equal("Client 1", result[0].Name);
+        Assert.Equal("Client 2", result[1].Name);
+        Assert.Equal("Client 3", result[2].Name);
     }
 }
