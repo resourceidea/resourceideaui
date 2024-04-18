ce485309e0507dbfccc1f64f84d5a177df833a8d
Joseph Serunjogi <joeseggie@gmail.com>
2024-04-18 14:44:55 +0300

    Add query to get subscription by Id

---
+++ src/dev/Core/EastSeat.ResourceIdea.Application/Features/Subscriptions/Handlers/GetSubscriptionByIdQueryHandler.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+
+using EastSeat.ResourceIdea.Application.Features.Common.Contracts;
+using EastSeat.ResourceIdea.Application.Features.Subscriptions.Queries;
+using EastSeat.ResourceIdea.Application.Features.Subscriptions.Specifications;
+using EastSeat.ResourceIdea.Domain.Common.Responses;
+using EastSeat.ResourceIdea.Domain.Subscriptions.Entities;
+using EastSeat.ResourceIdea.Domain.Subscriptions.Models;
+using EastSeat.ResourceIdea.Domain.Subscriptions.ValueObjects;
+
+using MediatR;
+
+using Optional;
+
+namespace EastSeat.ResourceIdea.Application.Features.Subscriptions.Handlers;
+
+public sealed class GetSubscriptionByIdQueryHandler(
+    IAsyncRepository<Subscription> subscriptionRepository,
+    IMapper mapper) : IRequestHandler<GetSubscriptionByIdQuery, ResourceIdeaResponse<SubscriptionModel>>
+{
+    private readonly IAsyncRepository<Subscription> _subscriptionRepository = subscriptionRepository;
+    private readonly IMapper _mapper = mapper;
+
+    public async Task<ResourceIdeaResponse<SubscriptionModel>> Handle(GetSubscriptionByIdQuery request, CancellationToken cancellationToken)
+    {
+        GetSubscriptionByIdSpecification getSubscriptionByIdSpecification = new (request.SubscriptionId);
+        Option<Subscription> getSubscriptionResult = await _subscriptionRepository.GetByIdAsync(getSubscriptionByIdSpecification, cancellationToken);
+        Subscription subscription = getSubscriptionResult.Match(
+            some: subscription => subscription,
+            none: () => EmptySubscription.Instance);
+
+        if (subscription == EmptySubscription.Instance)
+        {
+            return ResourceIdeaResponse<SubscriptionModel>.NotFound();
+        }
+
+        return new ResourceIdeaResponse<SubscriptionModel>
+        {
+            Success = true,
+            Content = Option.Some(_mapper.Map<SubscriptionModel>(subscription))
+        };
+    }
+}
---
+++ src/dev/Core/EastSeat.ResourceIdea.Application/Features/Subscriptions/Queries/GetSubscriptionByIdQuery.cs
@@ -0,0 +1,16 @@
+﻿using EastSeat.ResourceIdea.Domain.Common.Responses;
+using EastSeat.ResourceIdea.Domain.Subscriptions.Models;
+using EastSeat.ResourceIdea.Domain.Subscriptions.ValueObjects;
+
+using MediatR;
+
+namespace EastSeat.ResourceIdea.Application.Features.Subscriptions.Queries;
+
+/// <summary>
+/// Query to get a subscription by Id.
+/// </summary>
+public sealed class GetSubscriptionByIdQuery : IRequest<ResourceIdeaResponse<SubscriptionModel>>
+{
+    /// <summary>Subscription Id.</summary>
+    public SubscriptionId SubscriptionId { get; set; }
+}
