6e11eba077f9e29dc85115cfbd1082cc5941f9e1
Joseph Serunjogi <joeseggie@gmail.com>
2024-12-12 16:19:10 +0300

    Make changes more secure

---
+++ src/dev/Core/EastSeat.ResourceIdea.Domain/Enums/ErrorMessages.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EastSeat.ResourceIdea.Domain.Enums;
+
+/// <summary>
+/// Error messages.
+/// </summary>
+public enum ErrorMessages
+{
+    /// <summary>
+    /// Invalid URL format.
+    /// </summary>
+    InvalidUrlFormat
+}
