eecc321ed751fd0232eb539394570914db5aefb7
Joseph Serunjogi <joeseggie@gmail.com>
2025-01-10 12:09:57 +0300

    Remove AutoMapper from subscription operations (#291)
    Closes #283

--- src/dev/Core/EastSeat.ResourceIdea.Application/Features/Subscriptions/Handlers/GetSubscriptionByIdQueryHandler.cs
+++ src/dev/Core/EastSeat.ResourceIdea.Application/Features/Subscriptions/Handlers/GetSubscriptionByIdQueryHandler.cs
@@ -1,8 +1,9 @@
-﻿using AutoMapper;
-
+﻿using EastSeat.ResourceIdea.Application.Features.Common.Handlers;
 using EastSeat.ResourceIdea.Application.Features.Subscriptions.Contracts;
 using EastSeat.ResourceIdea.Application.Features.Subscriptions.Queries;
 using EastSeat.ResourceIdea.Application.Features.Subscriptions.Specifications;
+using EastSeat.ResourceIdea.Domain.Enums;
+using EastSeat.ResourceIdea.Domain.Subscriptions.Entities;
 using EastSeat.ResourceIdea.Domain.Subscriptions.Models;
 using EastSeat.ResourceIdea.Domain.Types;
 
@@ -10,18 +11,17 @@ using MediatR;
 
 namespace EastSeat.ResourceIdea.Application.Features.Subscriptions.Handlers;
 
-public sealed class GetSubscriptionByIdQueryHandler(
-    ISubscriptionsService subscriptionsService,
-    IMapper mapper) : IRequestHandler<GetSubscriptionByIdQuery, ResourceIdeaResponse<SubscriptionModel>>
+public sealed class GetSubscriptionByIdQueryHandler(ISubscriptionsService subscriptionsService)
+    : BaseHandler, IRequestHandler<GetSubscriptionByIdQuery, ResourceIdeaResponse<SubscriptionModel>>
 {
     private readonly ISubscriptionsService _subscriptionsService = subscriptionsService;
-    private readonly IMapper _mapper = mapper;
 
     public async Task<ResourceIdeaResponse<SubscriptionModel>> Handle(GetSubscriptionByIdQuery request, CancellationToken cancellationToken)
     {
         GetSubscriptionByIdSpecification getSubscriptionByIdSpecification = new (request.SubscriptionId);
         var response = await _subscriptionsService.GetByIdAsync(getSubscriptionByIdSpecification, cancellationToken);
+        var handlerResponse = GetHandlerResponse<Subscription, SubscriptionModel>(response, ErrorCode.NotFound);
 
-        return _mapper.Map<ResourceIdeaResponse<SubscriptionModel>>(response.Content.Value);
+        return handlerResponse;
     }
 }
