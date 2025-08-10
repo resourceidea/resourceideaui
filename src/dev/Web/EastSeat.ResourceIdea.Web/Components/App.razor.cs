3e56e3369eed571938fbe7e4cb4f4bd5d36b9549
Joseph Serunjogi <joeseggie@gmail.com>
2025-08-10 18:40:27 +0300

    Fix compile error

---
+++ src/dev/Web/EastSeat.ResourceIdea.Web/Components/App.razor.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Components;
+
+namespace EastSeat.ResourceIdea.Web.Components;
+
+// Partial class to ensure the App component type is available at compile-time
+public partial class App : ComponentBase
+{
+}
